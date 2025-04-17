8f930a9f89d5b9607e6ea7fdcdab156e87c9a31a
Tyler Teppema <tyler.teppema@brightfish.com>
2025-04-17 16:42:39 +0200

    changed nvim bin location

--- pkg/godvim/run.go
+++ pkg/godvim/run.go
@@ -15,16 +15,17 @@ func Run() {
         fmt.Println("This command should be run as ./godvim [IP] [port] [vim command]")
     }
 
-    ip := args[1]
-    port := args[2]
-    vim_commad := args[3]
+    nvim_bin_location := args[1]
+    ip := args[2]
+    port := args[3]
+    vim_commad := args[4]
 
     log.Println(fmt.Sprintf("Ip: %s, port: %s, vim_command: %s", ip, port, vim_commad))
 
     writeLog(fmt.Sprintf("Ip: %s, port: %s, vim_command: %s", ip, port, vim_commad))
 
     // nvim --server localhost:3456 --remote-send '<Esc><Space>d<C-w>w:e /tmp/test.go<CR>'
-    exec := exec.Command("/opt/homebrew/bin/nvim", "--server", ip + ":" + port, "--remote-send", vim_commad)
+    exec := exec.Command(nvim_bin_location, "--server", ip + ":" + port, "--remote-send", vim_commad)
 
     log.Println("Configured Command")
     writeLog("Configured Command")
